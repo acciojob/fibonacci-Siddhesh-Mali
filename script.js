313be7c3ff5fdcbee43914a949bde4e9c8bf3e0e
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-02-19 12:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let prevOne = 1;
+	let BeforePrevOne = 0;
+	let fib;
+
+	for (let i = 3; i <= num; i++) {
+		fib = prevOne + BeforePrevOne;
+		BeforePrevOne = prevOne;
+		prevOne = fib;
+	}
+	return fib;
 }
 
 module.exports = fibonacci;
